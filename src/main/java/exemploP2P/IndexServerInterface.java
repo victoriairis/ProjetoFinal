4cbafde26e4eb709e92117823da9e32e7da11396
Iris Roques <victoria.santana@ufersa.edu.br>
2020-07-23 21:35:50 -0300

    montando exemplo

---
+++ src/main/java/exemploP2P/IndexServerInterface.java
@@ -0,0 +1,16 @@
+package exemploP2P;
+
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+
+public interface IndexServerInterface extends Remote {
+    public boolean indexUpdate(PeerClientInterface newPeer) throws RemoteException;
+	public String addtoIndex(PeerClientInterface newPeer) throws RemoteException;
+	PeerClientInterface[] searchIndex(String file, String peerInitial) throws RemoteException;
+    //String[] getFiles() throws RemoteException;
+    //String peerArray() throws RemoteException;
+    
+}
---
+++ src/main/java/exemploPassingStream/ExemploStram.java
@@ -0,0 +1,15 @@
+package exemploPassingStream;
+
+import java.util.stream.Stream;
+
+public class ExemploStram {
+
+	public ExemploStram() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	//<byte[]> Stream<byte[]> map(Function<? super Agendamento, ? extends byte[]> mapper);
+	// https://www.geeksforgeeks.org/stream-map-java-examples/?ref=rp
+	// https://www.javacodegeeks.com/2015/02/pass-streams-instead-of-lists.html
+	// https://docs.oracle.com/javase/8/docs/api/java/util/stream/Collectors.html
+}
